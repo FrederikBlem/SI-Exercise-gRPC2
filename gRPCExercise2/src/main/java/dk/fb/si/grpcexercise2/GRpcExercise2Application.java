d21adebdab736662b78b4a8ce84e36c659e1585c
FrederikBlem <frederik_blem@hotmail.com>
2021-09-26 12:36:10 +0200

    Added the exercise project

---
+++ gRPCExercise2/src/main/java/dk/fb/si/grpcexercise2/GRpcExercise2Application.java
@@ -0,0 +1,13 @@
+package dk.fb.si.grpcexercise2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GRpcExercise2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GRpcExercise2Application.class, args);
+    }
+
+}
---
+++ gRPCExercise2/src/test/java/dk/fb/si/grpcexercise2/GRpcExercise2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.fb.si.grpcexercise2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcExercise2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
